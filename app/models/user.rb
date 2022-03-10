0f8b2da52033a2257d4edefacab75959b614bcb4
triznylaras <triznylaras@gmail.com>
2022-03-10 17:33:54 +0700

    add has_many posts function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :username, presence: true, length: { in: 4..14 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
